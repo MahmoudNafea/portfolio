0f5861d3836253c8d3470d4cf5b50aef800e750e
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-01-13 14:02:36 +0800

    filling the about page

--- front/src/components/about.js
+++ front/src/components/about.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import myphoto from './../../src/img/myphoto.jpg'
 
 const AboutPage = () => {
     return (
         <div>
-            <h1>Software developer</h1>
-            <p>27 years old</p>
+            <h1>About me</h1>
+            <p className="text">
+                Nice to see you. Let me introduce myself with the basic information about me.
+                 I'm Egyptian working now in Malaysia. I've bachelor's degree in science and I'm 27 years old.
+            </p>
+            <p className="text">
+                My experience is more than 3 years in technology and business industry.
+                 My current employer's Accenture Malaysia as I'm working as data analyst for two years so far.
+            </p>
+            <img src={myphoto} alt={myphoto} className="img" />
         </div>
     )
 }
--- front/src/components/login.js
+++ front/src/components/login.js
@@ -3,7 +3,20 @@ import React from 'react';
 const LoginPage = () => {
     return (
         <div>
-            <p>Login</p>
+            <h1>Please login</h1>
+            <form>
+                <div>
+                    <label>Username</label>
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button type="submit"> Login</button>
+            </form>
+
+            <button>Login with google</button>
         </div>
     )
 }
