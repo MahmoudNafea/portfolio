a2d485a641777e37807045748634f1e004de0053
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-01-20 11:16:13 +0800

    build

--- server/server.js
+++ server/server.js
@@ -3,17 +3,19 @@ const path = require('path')
 
 const app = express();
 
+const publicPath = path.join(__dirname, '', '../build')
+app.use(express.static(publicPath))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '', '../build')))
-    const publicPath = path.join(__dirname, '', '../public')
-    app.use(express.static(publicPath))
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '', '../build')))
+// }
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 })
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-    console.log('Server is up!');
+    console.log('Server is up on port ' + port);
 });
