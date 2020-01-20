d41b27fa58a2b53a895d849c89786494c422a7b1
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-01-20 09:15:32 +0800

    serve production and public

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
-const publicPath = path.join(__dirname, '', '../build')
+const publicPath = path.join(__dirname, '', '../public')
 app.use(express.static(publicPath))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("../build"))
+}
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
