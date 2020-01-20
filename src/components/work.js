e9758edf50bb49ba10f5b0b4b89ed04b20074bd9
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-01-20 13:42:10 +0800

    add my pre employers to work

--- src/components/work.js
+++ src/components/work.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import accenture from './../img/accenture.jpg'
+import spectrum from './../img/spectrum.jpg'
+import topelectric from './../img/topelectric.jpg'
+
 
 const WorkPage = () => {
     return (
         <div>
             <h2>My employers</h2>
             <div>
-
+                <a href="https://www.accenture.com/my-en">
+                    <img src={accenture} alt="icon" className="accenture" />
+                </a>
+                <a href="http://www.spectrum-diagnostics.com/new/">
+                    <img src={spectrum} alt="icon" className="accenture" />
+                </a>
+                <a href="">
+                    <img src={topelectric} alt="icon" className="accenture" />
+                </a>
             </div>
         </div>
     )
