6c0439a225b1e1b5cd615cb6fa80f53bb244fb95
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-01-28 08:57:05 +0800

    fixing span to self closing instead of open and close

--- src/components/ui/navbar.js
+++ src/components/ui/navbar.js
@@ -15,9 +15,9 @@ const Navbar = () => {
                 <nav className="navbar navbar-expand-sm navbar-light bg-light">
                     <div className="container">
                         <a className="home-style navbar-brand" href="/">PORTFOLIO</a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbarNav"
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
+                            <span className="navbar-toggler-icon" />
                         </button>
 
                         <div className="collapse navbar-collapse " id="navbarNav">
@@ -42,9 +42,7 @@ const Navbar = () => {
                 </nav>
 
             </div>
-            <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
         </React.Fragment>
     )
 }
