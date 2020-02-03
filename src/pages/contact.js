d9706ac7312ffb225c02e63173e15ff1bad9b2c8
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-02-03 11:00:20 +0800

    adding stackoverflow

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ContactForm from './../components/contactForm';
 import linkedin from './../img/linkedin.png';
 import github from './../img/github.png';
-import whats from './../img/whats.png'
+import stack from './../img/stack.jpg'
 import { Container, Image } from 'react-bootstrap';
 
 const ContactPage = () => {
@@ -21,15 +21,18 @@ const ContactPage = () => {
                 </div>
                 <div className="col-md-6">
                     <div>
-                        <h3 className="sub-title">Connect with me now</h3>
-                        <a href="https://www.linkedin.com/in/mahmoud-elsayed-nafea-a1478ba9/">
+                        <h3 className="sub-title" style={{ margin: "2rem " }}>Connect with me now</h3>
+                        <a href="https://www.linkedin.com/in/mahmoud-elsayed-nafea-a1478ba9/" style={{ margin: "1rem " }}>
                             <Image src={linkedin} fluid className="contact-img" />
                         </a>
-                        <a href="https://github.com/MahmoudNafea">
+                        <a href="https://github.com/MahmoudNafea" style={{ margin: "1rem " }}>
                             <Image src={github} fluid className="contact-img" />
                         </a>
+                        <a href="https://stackoverflow.com/users/12745027/mahmoud-nafea" style={{ margin: "1rem " }}>
+                            <Image src={stack} fluid className="contact-img" />
+                        </a>
                     </div>
-                    <div>
+                    <div style={{ marginTop: "2rem " }}>
                         <p>Tel: +60139381220</p>
                         <p>Gmail:mahmoudnafea19@gmail.com</p>
                     </div>
