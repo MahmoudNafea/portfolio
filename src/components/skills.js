dac7eedb42a4b9b02a74c250d8fc671c9772da72
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-02-04 12:37:27 +0800

    merging skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -18,36 +18,24 @@ const Skills = () => {
                         <Image src={python} fluid style={{ height: '12rem', width: '12rem' }} />
                     </div>
                     <div style={{ margin: "1rem " }}>
-                        <label>Excel</label>
-                        <ProgressBar now={90} label={`${90}%`} />
+                        <label>Excel Pivot Table </label>
+                        <ProgressBar now={80} label={`${80}%`} />
                     </div>
                     <div style={{ margin: "1rem " }}>
                         <label>Python</label>
                         <ProgressBar now={60} label={`${60}%`} />
                     </div>
                     <div style={{ margin: "1rem " }}>
-                        <label>Numpy</label>
+                        <label>Numpy, Pandas</label>
                         <ProgressBar now={90} label={`${90}%`} />
                     </div>
                     <div style={{ margin: "1rem " }}>
-                        <label>Pandas</label>
-                        <ProgressBar now={90} label={`${90}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>Plotly</label>
+                        <label>Matplotlib, Plotly, Seaborn</label>
                         <ProgressBar now={80} label={`${90}%`} />
                     </div>
                     <div style={{ margin: "1rem " }}>
-                        <label>Seaborn</label>
-                        <ProgressBar now={80} label={`${90}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>Linear Regression</label>
-                        <ProgressBar now={50} label={`${50}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>Logistic Regression</label>
-                        <ProgressBar now={50} label={`${50}%`} />
+                        <label>Linear Regression, Logistic Regression</label>
+                        <ProgressBar now={50} label={`${50}%`} animated />
                     </div>
                 </div>
                 <div className="col-md-6">
@@ -58,52 +46,24 @@ const Skills = () => {
                         <Image src={javascript} fluid style={{ height: '12rem', width: '12rem' }} />
                     </div>
                     <div style={{ margin: "1rem " }}>
-                        <label>Javascript</label>
-                        <ProgressBar now={75} label={`${75}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>Nodejs</label>
-                        <ProgressBar now={70} label={`${70}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>Reactjs</label>
+                        <label>Javascript, Nodejs, Reactjs</label>
                         <ProgressBar now={80} label={`${80}%`} />
                     </div>
                     <div style={{ margin: "1rem " }}>
-                        <label>HTML</label>
+                        <label>HTML, CSS, SASS, Bootstrap, JQuery</label>
                         <ProgressBar now={80} label={`${80}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>CSS,SASS and SCSS</label>
-                        <ProgressBar now={80} label={`${80}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>Bootstrap Responsive Design</label>
-                        <ProgressBar now={80} label={`${80}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>JQuery</label>
-                        <ProgressBar now={75} label={`${75}%`} />
                     </div>
                     <div style={{ margin: "1rem " }}>
                         <label>Database NO SQL and SOL</label>
                         <ProgressBar now={60} label={`${60}%`} />
                     </div>
                     <div style={{ margin: "1rem " }}>
-                        <label>Git and Github</label>
+                        <label>RESTFUL API, Git and Github</label>
                         <ProgressBar now={80} label={`${80}%`} />
                     </div>
                     <div style={{ margin: "1rem " }}>
-                        <label>RESTFUL API</label>
-                        <ProgressBar now={80} label={`${80}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>Cloud (Google,AWS,Heroku)</label>
-                        <ProgressBar now={80} label={`${80}%`} />
-                    </div>
-                    <div style={{ margin: "1rem " }}>
-                        <label>Photoshop</label>
-                        <ProgressBar now={40} label={`${40}%`} />
+                        <label>Cloud (Google,AWS,Heroku), Debugging</label>
+                        <ProgressBar now={80} label={`${80}%`} animated />
                     </div>
                 </div>
 
