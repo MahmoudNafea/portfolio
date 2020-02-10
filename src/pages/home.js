2b4dc630ff57a63583e60363b5057ca467e5c5c0
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-02-10 14:46:43 +0800

    adding animation to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
 class HomePage extends Component {
 
     render() {
         return (
             <div className="home">
+                <div id="stars" />
+                <div id="stars2" />
+                <div id="stars3" />
                 <div className="container text-center homePage " >
                     <h2 className="title">Hello, I'm Mahmoud Nafea</h2>
                     <h4 className="intro">Software developer && Data analyst </h4>
