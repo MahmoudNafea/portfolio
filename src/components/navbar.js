cbb4abea3e7f9770f1e643a1fb60674c63c34298
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-02-17 01:24:55 +0800

    SPA by using Link instead of href

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const NavbarPage = () => {
@@ -7,15 +8,15 @@ const NavbarPage = () => {
         <div>
             <Container >
                 <Navbar expand="sm"  >
-                    <Navbar.Brand href="/">Portfolio</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">Portfolio</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto ">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                            <Nav.Link href="/work">Work</Nav.Link>
-                            <Nav.Link href="/projects">Projects</Nav.Link>
-                            <Nav.Link href="/contact">Contact</Nav.Link>
+                        <Nav className="ml-auto " >
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/work">Work</Nav.Link>
+                            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
-// import 'bootstrap/scss/bootstrap.scss';
-// 
-
-
 import './styles/styles.scss';
-
 import 'normalize.css/normalize.css';
 import * as serviceWorker from './serviceWorker';
 
