9d31a7e319ee3c70835b48d39f1ce28d9401de07
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-03-22 21:21:53 +0800

    b

---
+++ build/precache-manifest.fb6b67fa38fbd256f459a00534779e91.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae62fe4fa5ac7fe8c55355549bf4c615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7695c27a72e66cd50bbd",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c23256154e9be4fb1274",
+    "url": "/static/css/main.ae52f586.chunk.css"
+  },
+  {
+    "revision": "7695c27a72e66cd50bbd",
+    "url": "/static/js/2.7c6db7e2.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.7c6db7e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c23256154e9be4fb1274",
+    "url": "/static/js/main.f5c72714.chunk.js"
+  },
+  {
+    "revision": "c9f1875f7e246a67df7e",
+    "url": "/static/js/runtime-main.cb82405f.js"
+  },
+  {
+    "revision": "1f77eaaadb22c1d83c2da0b8698fa69c",
+    "url": "/static/media/accenture.1f77eaaa.jpg"
+  },
+  {
+    "revision": "9c5c3df04d368f1d9ad274c4598f3e44",
+    "url": "/static/media/background.9c5c3df0.jpg"
+  },
+  {
+    "revision": "860436f09ddd5c35ebb6e1738b61fd2d",
+    "url": "/static/media/burger.860436f0.PNG"
+  },
+  {
+    "revision": "d575ba0664fcfc1360dbc98e50401e46",
+    "url": "/static/media/fitness.d575ba06.PNG"
+  },
+  {
+    "revision": "957c4dd9aa815cc1de2040cc1259589f",
+    "url": "/static/media/github.957c4dd9.png"
+  },
+  {
+    "revision": "93e1a5c4c74c7cf85427a4bb432685f5",
+    "url": "/static/media/googleAnalysis.93e1a5c4.PNG"
+  },
+  {
+    "revision": "581be8f66c134cc1b6fedaf636da1516",
+    "url": "/static/media/indecision.581be8f6.PNG"
+  },
+  {
+    "revision": "b4cc9b6dc76bcc06f3d83656720ebde7",
+    "url": "/static/media/javascript.b4cc9b6d.png"
+  },
+  {
+    "revision": "bf1a29195623748ed389d026482dc25e",
+    "url": "/static/media/linkedin.bf1a2919.png"
+  },
+  {
+    "revision": "b11446ea105f131201c5640d71834927",
+    "url": "/static/media/myphoto.b11446ea.jpg"
+  },
+  {
+    "revision": "3964a919fd852fd26979eeedfc03b8d7",
+    "url": "/static/media/python.3964a919.png"
+  },
+  {
+    "revision": "8456a80922014e1a0d488ff097a9a15a",
+    "url": "/static/media/reactHooks.8456a809.PNG"
+  },
+  {
+    "revision": "bb3d86068751c3d5b45efe6846d90420",
+    "url": "/static/media/spectrum.bb3d8606.jpg"
+  },
+  {
+    "revision": "349e75fd6dc15d1b30f4f88746939cf0",
+    "url": "/static/media/stack.349e75fd.jpg"
+  },
+  {
+    "revision": "5ba504f69dc3942494b6a2978589d001",
+    "url": "/static/media/topelectric.5ba504f6.jpg"
+  },
+  {
+    "revision": "11b54263ff407f6453bd1455fa97ffc8",
+    "url": "/static/media/weather.11b54263.PNG"
+  }
+]);
