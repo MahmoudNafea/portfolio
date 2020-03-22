6fb6a2174922a5319ce1376c8127ae649ab22017
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-03-22 21:09:01 +0800

    path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const publicPath = path.join(__dirname, "", "client/build");
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+  res.sendFile(publicPath, "index.html");
 });
 const port = process.env.PORT || 3000;
 
